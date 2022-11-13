05f2fca7248044564ef9020f01ecdeddad77115b
Neauangle <eulerspill@protonmail.com>
2022-11-13 13:23:45 +1000

    improved bat to always install (update)

--- run.bat
+++ run.bat
@@ -3,18 +3,15 @@ REM Change to batch file's location so we can call it anywhere
 CD /D "%~dp0" 
 
 cd telegram_bot
-if not exist "node_modules" (
-	echo Installing telegram_bot dependencies...
-    CALL npm install
-	echo: 
-)
+echo Installing telegram_bot dependencies...
+CALL npm install
+echo: 
+
 
 cd ../verification_site
-if not exist "node_modules" (
-	echo Installing verification site dependencies...
-    CALL npm install
-	echo: 
-)
+echo Installing verification site dependencies...
+CALL npm install
+echo: 
 
 cd ..
 
