84bc58e2a1ea50665e2ce9fb42a96941e98f93aa
Neauangle <eulerspill@protonmail.com>
2022-11-13 12:24:03 +1000

    added request_free and request_pro implementing wallet balance checks

--- telegram_bot/bot.js
+++ telegram_bot/bot.js
@@ -1,19 +1,28 @@
 import TelegramBot from 'node-telegram-bot-api';
 import fs from 'fs';
 import crypto from 'crypto';
-import ethers from 'ethers';
+import botiq from 'botiq';
 import net from 'net';
 
+
 //This lets us start the server from any folder and still use relative urls in here
-import process from 'process';
+import process, { config } from 'process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 process.chdir(__dirname);
 //------------------------------
 
+const configs = JSON.parse(fs.readFileSync('./user-config.json'));
 
-const VERIFICATION_CODE_LENGTH = 6;
+const ethereumEndpoint = await botiq.ethers.createJsonRpcEndpoint({
+    accessURL: configs.ethereumEndpoint.accessURL,
+    rateLimitPerSecond: configs.ethereumEndpoint.rateLimitPerSecond,
+    omitNativeTokenTrackerInit: true
+});
+
+const verificationCodeToUser = {};
+const userIdToInfo = {};
 
 const acceptedCommands = [
     {
@@ -43,34 +52,94 @@ const acceptedCommands = [
         description: `Requests a verification code to use in associating a telegram account with an ethereum wallet.`,
         handler: async (bot, message, matchResult) => {
             let verificationCode = '';
-            for (let i = 0; i < VERIFICATION_CODE_LENGTH; ++i){
+            for (let i = 0; i < config.verificationCodeLength; ++i){
                 verificationCode += crypto.randomInt(0, 10)
             }
             verificationCodeToUser[verificationCode] = message.from;
 
             bot.sendMessage(message.chat.id, `Here is your verification code: ${verificationCode}.`);       
         }
+    },
+    {
+        regex: /^\/request_free$/i,
+        usage: `/request_free`,
+        description: `Requests an AI-generated image using the free tier (requires at least ${config.freeTierMinimumBalance} duckereum in wallet).`,
+        handler: async (bot, message, matchResult) => {
+            const userInfo = userIdToInfo[message.from.id];
+            if (!userInfo){
+                bot.sendMessage(message.chat.id, `Error: There is no wallet address associated with your telegram yet!`);   
+                return;
+            }
+            //todo: throttling - depends on what exactly you want the logic to be.
+
+            try {
+                const duckereumBalance = await ethereumEndpoint.getBalance({
+                    walletAddress: userInfo.walletAddress, 
+                    tokenAddress: config.freeTierTokenAddress,
+                });
+                if (!duckereumBalance.rational.greaterOrEquals(config.freeTierMinimumBalance)){
+                    bot.sendMessage(message.chat.id, `Error: Use of the free tier requires at least ${config.freeTierMinimumBalance} duckereum in your wallet.\nBalance of wallet ${userInfo.walletAddress.slice(0, 8)}: ${duckereumBalance.string}.`);
+                    return;
+                } 
+            } catch (error){
+                console.log(error);
+                bot.sendMessage(message.chat.id, 'Error: Failed checking duckereum balance. Plase try again later 🙏.');
+                return;
+            }
+
+            bot.sendMessage(message.chat.id, 'Here is your image: 🐤');
+        }
+    },
+    {
+        regex: /^\/request_pro$/i,
+        usage: `/request_pro`,
+        description: `Requests an AI-generated image using the pro tier.`,
+        handler: async (bot, message, matchResult) => {
+            const userInfo = userIdToInfo[message.from.id];
+            if (!userInfo){
+                bot.sendMessage(message.chat.id, `Error: There is no wallet address associated with your telegram yet!`);   
+                return;
+            }
+
+            try {
+                const nftBalance = await ethereumEndpoint.getBalance({
+                    walletAddress: userInfo.walletAddress, 
+                    tokenAddress: config.proTierNftAddress,
+                    isNft: true
+                });
+                if (!nftBalance.rational.greater(0)){
+                    bot.sendMessage(message.chat.id, `Error: No pro-tier NFT found in wallet ${userInfo.walletAddress.slice(0, 8)}.`);
+                    return;
+                } 
+            } catch (error){
+                console.log(error);
+                bot.sendMessage(message.chat.id, 'Error: Failed checking pro-tier NFT balance. Plase try again later 🙏.');
+                return;
+            }
+
+            bot.sendMessage(message.chat.id, 'Here is your image: 🐤');
+        }
     }
 ];
 
 
-const verificationCodeToUser = {};
-const userIdToInfo = {};
 
-const configs = JSON.parse(fs.readFileSync('./user-config.json'));
 
 await run(configs);
 
 
 export async function run(configs){
-    const bot = new TelegramBot(configs.telegramAccessToken, {polling: true});
     
+    console.log("Starting bot...")
+    const bot = new TelegramBot(configs.telegramAccessToken, {polling: true});
     const commands = [];
     for (const acceptedCommand of acceptedCommands){
         commands.push({command: acceptedCommand.usage.split(' ')[0].slice(1), description: acceptedCommand.description});
     }
     await bot.setMyCommands(commands);
-    console.log("Bot started!");
+    bot.me = await bot.getMe();
+    console.log(`@${bot.me.username} is live!`);
+    
 
     bot.on('message', async (message) => {
         for (const acceptedCommand of acceptedCommands){
@@ -93,47 +162,49 @@ export async function run(configs){
             const json = JSON.parse(jsonString);
             const websocketId = json.websocketId;
             const data = JSON.parse(json.dataString);
-            const signedBy = ethers.utils.verifyMessage(data.verificationCode, data.verificationCodeSigned);
-            if (isHexEqual(signedBy, data.account)){
-                if (verificationCodeToUser[data.verificationCode]){
-                    const info = {
-                        user: verificationCodeToUser[data.verificationCode],
-                        walletAddress: signedBy
-                    }
-                    userIdToInfo[verificationCodeToUser[data.verificationCode].id] = info;
-                    bot.sendMessage(verificationCodeToUser[data.verificationCode].id, `You wallet has been verified as: ${signedBy}`);
-                    socket.write(JSON.stringify({
-                        websocketId,
-                        dataString: JSON.stringify({
-                            username: info.user.username,
-                            walletAddress: info.walletAddress
-                        })
-                    }));
-                } else {
-                    socket.write(JSON.stringify({
-                        websocketId,
-                        dataString: JSON.stringify({
-                            error: `No pending verification matching ${data.verificationCode}. Please get a new one from the telegram bot!` 
-                        })
-                    }));
-                }
-            } else {
+            
+            if (!verificationCodeToUser[data.verificationCode]){
+                socket.write(JSON.stringify({
+                    websocketId,
+                    dataString: JSON.stringify({
+                        error: `No pending verification matching ${data.verificationCode}. Please get a new one from @${bot.me.username}` 
+                    })
+                }));
+                return;
+            }
+
+            const user = verificationCodeToUser[data.verificationCode];
+            const signedBy = botiq.ethers.ethers.utils.verifyMessage(data.verificationCode, data.verificationCodeSigned);
+            if (!botiq.util.isHexEqual(signedBy, data.account)){
+                bot.sendMessage(user.id, "Error: Authentication of wallet address failed...");
                 socket.write(JSON.stringify({
                     websocketId,
                     dataString: JSON.stringify({
                         error: "Authentication of wallet address failed..."
                     })
                 }));
+                delete verificationCodeToUser[data.verificationCode];
+                return;
             }
+            
+            const info = {
+                user: verificationCodeToUser[data.verificationCode],
+                walletAddress: signedBy
+            }
+            userIdToInfo[verificationCodeToUser[data.verificationCode].id] = info;
+            bot.sendMessage(verificationCodeToUser[data.verificationCode].id, `Success! Your wallet has been verified as: ${signedBy}`);
+            socket.write(JSON.stringify({
+                websocketId,
+                dataString: JSON.stringify({
+                    username: info.user.username,
+                    walletAddress: info.walletAddress
+                })
+            }));
         });
     });
     walletVerificationsServer.listen(configs.walletVerificationsPort, () => {
-        console.log(`Listening for wallet verifications on localhost port ${configs.walletVerificationsPort}.\n`);
+        console.log(`Listening for wallet verifications on localhost port ${configs.walletVerificationsPort}.`);
     });
 
 }
 
-
-function isHexEqual(hexA, hexB){
-    return ethers.utils.hexValue(hexA) === ethers.utils.hexValue(hexB);
-}
